74af7b4ecc391ad26c5481c3d58dd5343fb0aefc
william.jiang <william.jiang@vungle.com>
2024-09-30 22:11:55 +0800

    feat: add new page

---
+++ assets/NotFound-6857d022.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-efa2a47a.js";const o=()=>r("div",{children:"404"});export{o as default};
--- assets/NotFound-9a4fe8da.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-2849c33b.js";const o=()=>r("div",{children:"404"});export{o as default};
